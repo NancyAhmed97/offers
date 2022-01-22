e527e4852eb3a89cc0675029df905847aef3bd46
NancyAhmed97 <anancyahmed97@icloud.com>
2022-01-22 12:41:12 -0800

    add DealsOfTheWeek

--- src/Modules/Home/Deals/Deals.js
+++ src/Modules/Home/Deals/Deals.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import DealsOfTheWeek from './Components/DealsOfTheWeek/DealsOfTheWeek';
 import PopularItems from './Components/PopularItems/PopularItems';
 
 function Deals() {
   return <div className='deals pl pr mt-5 pt-3'>
 <Container className='m-0 p-0' fluid>
     <Row className='m-0 p-0'>
-        <Col md={3} className='m-0 p-0'>hi</Col>
+        <Col md={3} className='m-0 p-0'>
+          <DealsOfTheWeek />
+        </Col>
         <Col md={9} className='m-0 p-0'>
           <PopularItems />
         </Col>
