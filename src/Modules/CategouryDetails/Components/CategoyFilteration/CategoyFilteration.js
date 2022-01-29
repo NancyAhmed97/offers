aef4c36adb89b0deee1a407daf98d02fec3f1c3e
NancyAhmed97 <anancyahmed97@icloud.com>
2022-01-29 13:17:29 -0800

    category

--- src/Modules/CategouryDetails/Components/CategoyFilteration/CategoyFilteration.js
+++ src/Modules/CategouryDetails/Components/CategoyFilteration/CategoyFilteration.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import CategoryProduct from './Components/CategoryProduct/CategoryProduct';
 import FilerBar from './Components/FilerBar/FilerBar';
 
 function CategoyFilteration() {
   return <div className='categoy_filteration pr pl'>
-      <FilerBar />
-      <CategoryProduct />
-      CategoyFilteration ghgfh
+      <Container fluid className='p-0 m-0'>
+          <Row className='p-0 m-0'>
+              <Col className='p-0' md={3}>
+                  <FilerBar />
+              </Col>
+              <Col className='p-0' md={9}>
+                  <CategoryProduct />
+              </Col>
+          </Row>
+      </Container>
+
   </div>;
 }
 
