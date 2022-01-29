d916f4378733da047c31565af0cc1b4374adadb4
NancyAhmed97 <anancyahmed97@icloud.com>
2022-01-29 12:58:14 -0800

    add

--- src/Modules/CategouryDetails/CategouryDetails.js
+++ src/Modules/CategouryDetails/CategouryDetails.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Footer from '../Common/Footer/Footer';
 import Navbar from '../Common/Navbar/Navba';
 import HeaderOfCategory from './Components/HeaderOfCategory/HeaderOfCategory';
+import CategoyFilteration from "./Components/CategoyFilteration/CategoyFilteration"
 function CategouryDetails() {
   return <section>
       <Navbar />
       <HeaderOfCategory />
+      <CategoyFilteration />
       <Footer />
   </section>;
 }
---
+++ src/Modules/CategouryDetails/Components/CategoyFilteration/CategoyFilteration.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CategoyFilteration() {
+  return <div className='categoy_filteration pr pl'>
+      CategoyFilteration ghgfh
+  </div>;
+}
+
+export default CategoyFilteration;
