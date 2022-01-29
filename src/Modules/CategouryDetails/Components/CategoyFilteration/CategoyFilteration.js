e3e121ffcb1610efef9bf28f0d8665b94855f230
NancyAhmed97 <anancyahmed97@icloud.com>
2022-01-29 13:06:56 -0800

    add FilerBar

--- src/Modules/CategouryDetails/Components/CategoyFilteration/CategoyFilteration.js
+++ src/Modules/CategouryDetails/Components/CategoyFilteration/CategoyFilteration.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import FilerBar from './Components/FilerBar/FilerBar';
 
 function CategoyFilteration() {
   return <div className='categoy_filteration pr pl'>
+      <FilerBar />
       CategoyFilteration ghgfh
   </div>;
 }
---
+++ src/Modules/CategouryDetails/Components/CategoyFilteration/Components/FilerBar/FilerBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./FilerBar.css"
+function FilerBar() {
+  return <div className='filer_bar'>
+      FilerBar
+  </div>;
+}
+
+export default FilerBar;
