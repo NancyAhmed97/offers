82d827ca8c921687100082b9b9fda0c40d08d80e
NancyAhmed97 <anancyahmed97@icloud.com>
2022-01-29 13:11:33 -0800

    CategoryProduct

--- src/Modules/CategouryDetails/Components/CategoyFilteration/CategoyFilteration.js
+++ src/Modules/CategouryDetails/Components/CategoyFilteration/CategoyFilteration.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CategoryProduct from './Components/CategoryProduct/CategoryProduct';
 import FilerBar from './Components/FilerBar/FilerBar';
 
 function CategoyFilteration() {
   return <div className='categoy_filteration pr pl'>
       <FilerBar />
+      <CategoryProduct />
       CategoyFilteration ghgfh
   </div>;
 }
---
+++ src/Modules/CategouryDetails/Components/CategoyFilteration/Components/CategoryProduct/CategoryProduct.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CategoryProduct() {
+  return <div className='category_product'>
+      CategoryProduct
+  </div>;
+}
+
+export default CategoryProduct;
