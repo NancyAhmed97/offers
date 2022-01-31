bff0bede94bed34a560813668de394a0b8ea7b7b
NancyAhmed97 <anancyahmed97@icloud.com>
2022-01-31 14:08:59 +0200

    fom

--- src/Modules/Common/Navbar/TapBar/TapBar.js
+++ src/Modules/Common/Navbar/TapBar/TapBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import FaceBook from "../../../../Resources/Assets/img/FaceBook"
+import FaceBook from "../../../../Resources/Assets/img/FaceBook";
 import Instgram from "../../../../Resources/Assets/img/Instgram";
 import Snapchat from "../../../../Resources/Assets/img/Snapchat";
 import Twiiter from "../../../../Resources/Assets/img/Twiiter";
@@ -12,12 +12,19 @@ import { useSelector, useDispatch } from "react-redux";
 import { changeLocal } from "../../../../Redux/Localization";
 
 import "./TapBar.css";
+import { Link } from "react-router-dom";
 function TapBar() {
   const { currentLocal } = useSelector((state) => state.currentLocal);
   const dispatch = useDispatch();
 
   return (
-    <div className={currentLocal.language==="English"?"top_bar pr pl":"top_bar pr pl ar_top_bar"}>
+    <div
+      className={
+        currentLocal.language === "English"
+          ? "top_bar pr pl"
+          : "top_bar pr pl ar_top_bar"
+      }
+    >
       <div className="top_bar_container">
         <div className="top_bar_left mt-1">
           <ul className="social_media">
@@ -27,12 +34,12 @@ function TapBar() {
                 target="_blank"
                 rel="noreferrer"
               >
-<FaceBook />
-         </a>
+                <FaceBook />
+              </a>
             </li>
             <li className="d-inline-block mx-3">
               <a href="https://twitter.com/" target="_blank" rel="noreferrer">
-<Twiiter />
+                <Twiiter />
               </a>
             </li>
             <li className="d-inline-block">
@@ -41,7 +48,7 @@ function TapBar() {
                 target="_blank"
                 rel="noreferrer"
               >
-<Instgram />
+                <Instgram />
               </a>
             </li>
             <li className="d-inline-block mx-3">
@@ -50,7 +57,7 @@ function TapBar() {
                 target="_blank"
                 rel="noreferrer"
               >
-<Snapchat />
+                <Snapchat />
               </a>
             </li>{" "}
             <li className="d-inline-block">
@@ -59,7 +66,7 @@ function TapBar() {
                 target="_blank"
                 rel="noreferrer"
               >
-<Youtube />
+                <Youtube />
               </a>
             </li>
           </ul>
@@ -69,48 +76,75 @@ function TapBar() {
           <img src={support} alt="support" />
         </div>
         <div className="top_bar_right">
-        <div className="langouage">
-              <Dropdown>
-                <Dropdown.Toggle
-                  variant="transparent"
-                  className="text-white"
-                  style={{display:"flex",alignItems:"center",marginTop:"5px"}}
-                  id="English"
-                >
-                  <div>
-                  {currentLocal.language === "اللغه العربيه"
-                      ?<>
-                      <img src={saudi_arabia} alt="saudi_arabia" className="ml-3" />
+          <div className="langouage">
+            <Dropdown>
+              <Dropdown.Toggle
+                variant="transparent"
+                className="text-white"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginTop: "5px",
+                }}
+                id="English"
+              >
+                <div>
+                  {currentLocal.language === "اللغه العربيه" ? (
+                    <>
+                      <img
+                        src={saudi_arabia}
+                        alt="saudi_arabia"
+                        className="ml-3"
+                      />
                       <span className="mx-3">اللغه العربيه</span>
-                      </>
-                      : "English"}                  </div>
-                </Dropdown.Toggle>
+                    </>
+                  ) : (
+                    "English"
+                  )}{" "}
+                </div>
+              </Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item
-                    onClick={(e) => {
-                      dispatch(
-                        changeLocal(
-                          currentLocal.language === "English" ? "ar" : "en"
-                        )
-                      );
-                    }}
-                    id="Arabic"
-                  >
-                    {currentLocal.language === "English"
-                      ?<>
-                      <img src={saudi_arabia} alt="saudi_arabia" className="ml-3" />
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  onClick={(e) => {
+                    dispatch(
+                      changeLocal(
+                        currentLocal.language === "English" ? "ar" : "en"
+                      )
+                    );
+                  }}
+                  id="Arabic"
+                >
+                  {currentLocal.language === "English" ? (
+                    <>
+                      <img
+                        src={saudi_arabia}
+                        alt="saudi_arabia"
+                        className="ml-3"
+                      />
                       <span className="mx-3">اللغه العربيه</span>
-                      </>
-                      : "English"}
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </div>
-       <div className="order">
-       <p className="text-white d-inline-block mx-3">{currentLocal.footer.track} </p>
-          <img src={order} alt="order" />
-       </div>
+                    </>
+                  ) : (
+                    "English"
+                  )}
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+          <div className="order">
+            <Link
+                  to="/trackorder"
+                  className="text-decoration-none"
+              onClick={() => {
+                window.scrollTo(0, 0);
+              }}
+            >
+              <p className="text-white d-inline-block mx-3">
+                {currentLocal.footer.track}{" "}
+              </p>
+              <img src={order} alt="order" />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
